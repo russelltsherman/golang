96df41a12923d0edd4711c34487df95ef7a02fd3
Russell <russell.t.sherman@gmail.com>
2020-04-28 22:33:38 -0500

    math

---
+++ math/go.mod
@@ -0,0 +1,3 @@
+module github.com/burnercodeschool/golang/math
+
+go 1.14
---
+++ math/math.go
@@ -0,0 +1,14 @@
+package math
+
+// Sum - return the sum of two numbers
+func Sum(x int, y int) int {
+	return x + y
+}
+
+// Fib - returns the nth number in the Fibonacci series.
+func Fib(n int) int {
+	if n < 2 {
+		return n
+	}
+	return Fib(n-1) + Fib(n-2)
+}
