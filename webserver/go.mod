a97f257bd6d6bc732afc1ed3279f1eeefd365fb2
Russell <russell.t.sherman@gmail.com>
2020-04-28 22:33:45 -0500

    webserver

---
+++ webserver/go.mod
@@ -0,0 +1,5 @@
+module github.com/burnercodeschool/golang/webserver
+
+go 1.14
+
+require github.com/lib/pq v1.4.0
---
+++ webserver/webserver_test.go
@@ -0,0 +1,147 @@
+package webserver
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+func connStr() string {
+	return "user=postgres password=supersecret dbname=s2 sslmode=disable"
+}
+
+func createTable() {
+	db, err := sql.Open("postgres", connStr())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	const query = `
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      first_name TEXT,
+      last_name TEXT)`
+
+	_, err = db.Exec(query)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	db.Close()
+}
+
+func dropTable() {
+	db, err := sql.Open("postgres", connStr())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_, err = db.Exec("DROP TABLE IF EXISTS users")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	db.Close()
+}
+
+func insertRecord(query string) {
+	db, err := sql.Open("postgres", connStr())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = db.Exec(query)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	db.Close()
+}
+
+func Test_count(t *testing.T) {
+	var count int
+	createTable()
+
+	insertRecord("INSERT INTO users (first_name, last_name) VALUES ('Epifanios', 'Doe')")
+	insertRecord("INSERT INTO users (first_name, last_name) VALUES ('Mihalis', 'Tsoukalos')")
+	insertRecord("INSERT INTO users (first_name, last_name) VALUES ('Mihalis', 'Unknown')")
+
+	db, err := sql.Open("postgres", connStr())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	row := db.QueryRow("SELECT COUNT(*) FROM users")
+	err = row.Scan(&count)
+	db.Close()
+
+	if count != 3 {
+		t.Errorf("Select query returned %d", count)
+	}
+	dropTable()
+}
+
+func Test_queryDB(t *testing.T) {
+	createTable()
+
+	db, err := sql.Open("postgres", connStr())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	query := "INSERT INTO users (first_name, last_name) VALUES ('Random Text', '123456')"
+	insertRecord(query)
+
+	rows, err := db.Query(`SELECT * FROM users WHERE last_name=$1`, `123456`)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	var col1 int
+	var col2 string
+	var col3 string
+	for rows.Next() {
+		rows.Scan(&col1, &col2, &col3)
+	}
+	if col2 != "Random Text" {
+		t.Errorf("first_name returned %s", col2)
+	}
+
+	if col3 != "123456" {
+		t.Errorf("last_name returned %s", col3)
+	}
+
+	db.Close()
+	dropTable()
+}
+
+func Test_record(t *testing.T) {
+	createTable()
+	insertRecord("INSERT INTO users (first_name, last_name) VALUES ('John', 'Doe')")
+
+	req, err := http.NewRequest("GET", "/getdata", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getData)
+	handler.ServeHTTP(rr, req)
+
+	status := rr.Code
+	if status != http.StatusOK {
+		t.Errorf("Handler returned %v", status)
+	}
+
+	if rr.Body.String() != "<h3 align=\"center\">1, John, Doe</h3>\n" {
+		t.Errorf("Wrong server response!")
+	}
+	dropTable()
+}
