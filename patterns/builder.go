078e0319219cf467345840c30b6740fe530d6cf0
Russell <russell.t.sherman@gmail.com>
2020-05-16 18:30:45 -0500

    add pattern implementations

--- patterns/builder.go
+++ patterns/builder.go
@@ -1,1 +1,115 @@
 package patterns
+
+// BuildProcess interface
+type BuildProcess interface {
+	SetWheels() BuildProcess
+	SetSeats() BuildProcess
+	SetStructure() BuildProcess
+	GetVehicle() VehicleProduct
+}
+
+// ManufacturingDirector struct
+type ManufacturingDirector struct {
+	builder BuildProcess
+}
+
+// Construct ManufacturingDirector
+func (f *ManufacturingDirector) Construct() {
+	f.builder.SetSeats().SetStructure().SetWheels()
+}
+
+// SetBuilder ManufacturingDirector
+func (f *ManufacturingDirector) SetBuilder(b BuildProcess) {
+	f.builder = b
+}
+
+// VehicleProduct struct
+type VehicleProduct struct {
+	Wheels    int
+	Seats     int
+	Structure string
+}
+
+// Builder of type car
+type CarBuilder struct {
+	v VehicleProduct
+}
+
+// SetWheels setter
+func (c *CarBuilder) SetWheels() BuildProcess {
+	c.v.Wheels = 4
+	return c
+}
+
+// SetSeats setter
+func (c *CarBuilder) SetSeats() BuildProcess {
+	c.v.Seats = 5
+	return c
+}
+
+// SetStructure setter
+func (c *CarBuilder) SetStructure() BuildProcess {
+	c.v.Structure = "Car"
+	return c
+}
+
+// GetVehicle getter
+func (c *CarBuilder) GetVehicle() VehicleProduct {
+	return c.v
+}
+
+//A Builder of type motorbike
+type BikeBuilder struct {
+	v VehicleProduct
+}
+
+// SetWheels setter
+func (b *BikeBuilder) SetWheels() BuildProcess {
+	b.v.Wheels = 2
+	return b
+}
+
+// SetSeats setter
+func (b *BikeBuilder) SetSeats() BuildProcess {
+	b.v.Seats = 2
+	return b
+}
+
+// SetStructure setter
+func (b *BikeBuilder) SetStructure() BuildProcess {
+	b.v.Structure = "Motorbike"
+	return b
+}
+
+// GetVehicle getter
+func (b *BikeBuilder) GetVehicle() VehicleProduct {
+	return b.v
+}
+
+// BusBuilder struct
+type BusBuilder struct {
+	v VehicleProduct
+}
+
+// SetWheels setter
+func (b *BusBuilder) SetWheels() BuildProcess {
+	b.v.Wheels = 8
+	return b
+}
+
+// SetSeats setter
+func (b *BusBuilder) SetSeats() BuildProcess {
+	b.v.Seats = 30
+	return b
+}
+
+// SetStructure setter
+func (b *BusBuilder) SetStructure() BuildProcess {
+	b.v.Structure = "Bus"
+	return b
+}
+
+// GetVehicle getter
+func (b *BusBuilder) GetVehicle() VehicleProduct {
+	return b.v
+}
--- patterns/factory.go
+++ patterns/factory.go
@@ -1,1 +1,58 @@
 package patterns
+
+import (
+	"errors"
+	"fmt"
+)
+
+//PaymentMethod defines a way of paying in the shop. This factory method returns
+//objects that implements this interface
+type PaymentMethod interface {
+	Pay(amount float32) string
+}
+
+//Our current implemented Payment methods are described here
+const (
+	Cash      = 1
+	DebitCard = 2
+)
+
+// CreatePaymentMethod returns a pointer to a PaymentMethod object or an error
+//if the method is not registered. We used "new" operator to return the pointer
+//but we could also used &Type{} althought new makes it more readable for
+//newcomers could be confusing
+
+// GetPaymentMethod function
+func GetPaymentMethod(m int) (PaymentMethod, error) {
+	switch m {
+	case Cash:
+		return new(CashPM), nil
+	case DebitCard:
+		return new(NewDebitCardPM), nil
+	default:
+		return nil, errors.New(fmt.Sprintf("Payment method %d not recognized\n", m))
+	}
+}
+
+// CashPM struct
+type CashPM struct{}
+
+// DebitCardPM struct
+type DebitCardPM struct{}
+
+// Pay method
+func (c *CashPM) Pay(amount float32) string {
+	return fmt.Sprintf("%0.2f payed using cash\n", amount)
+}
+
+// Pay method
+func (c *DebitCardPM) Pay(amount float32) string {
+	return fmt.Sprintf("%#0.2f payed using debit card\n", amount)
+}
+
+// NewDebitCardPM struct
+type NewDebitCardPM struct{}
+
+func (d *NewDebitCardPM) Pay(amount float32) string {
+	return fmt.Sprintf("%#0.2f payed using debit card (new)\n", amount)
+}
